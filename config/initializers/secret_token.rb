85942c2605cb5eeb492977d6d5f9897daae66728
em-ak <emilien.ak@gmail.com>
2014-01-17 19:16:35 +0100

    Rails initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Simploscope::Application.config.secret_key_base = '677034e54afe372f8409906a7f2690b47ca5e356efb5d414c7dc50f2f08a9279429e0315ff9815c37603fba9ce0c984f8932cc5827bb81143a3a9ad1d16c90a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Simploscope::Application.config.session_store :cookie_store, key: '_simploscope_session'
